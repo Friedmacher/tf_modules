12992392c7113b2016dfc60452c4194a99670e8a
Manuel Friedmacher <manuel.friedmacher@outlook.com>
2026-06-14 08:45:29 +0000

    fix: use other resource for changing defautl idp

--- modules/idp_trust/main.tf
+++ modules/idp_trust/main.tf
@@ -22,8 +22,14 @@ resource "btp_subaccount_trust_configuration" "create_trust" {
  * Disable SAP default IDP for user logon, so that users can only logon via the configured IAS IDP
   This is required to avoid that users can logon with their SAP S-User credentials, which are not managed in IAS
  */
-resource "btp_subaccount_trust_configuration" "change_default_idp" {
-  subaccount_id            = var.subaccount_id
-  identity_provider        = "sap.default"
-  available_for_user_logon = false
+resource "btp_subaccount_security_settings" "change_default_idp" {
+  subaccount_id                        = var.subaccount_id
+  default_idp_available_for_user_logon = false
 }
+
+
+#resource "btp_subaccount_trust_configuration" "change_default_idp" {
+#  subaccount_id            = var.subaccount_id
+#  identity_provider        = "sap.default"
+#  available_for_user_logon = false
+#}
