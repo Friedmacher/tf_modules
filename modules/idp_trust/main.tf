4453fdc6b7ab15aa1cb906bce8741cd9b121ef27
Manuel Friedmacher <manuel.friedmacher@outlook.com>
2026-06-14 09:35:19 +0000

    update: rollback default idp change

--- modules/idp_trust/main.tf
+++ modules/idp_trust/main.tf
@@ -16,29 +16,3 @@ resource "btp_subaccount_trust_configuration" "create_trust" {
   link_text                = "Identity provider for business users"
   status                   = "active"
 }
-
-/*
- * Disable SAP default IDP for user logon, so that users can only logon via the configured IAS IDP
-  This is required to avoid that users can logon with their SAP S-User credentials, which are not managed in IAS
- */
-import {
-  to = btp_subaccount_trust_configuration.default_idp
-  id = "${var.subaccount_id},sap.default"
-}
-resource "btp_subaccount_trust_configuration" "default_idp" {
-  subaccount_id            = var.subaccount_id
-  identity_provider        = "sap.default"
-  available_for_user_logon = false
-}
-resource "btp_subaccount_security_settings" "change_default_idp" {
-  subaccount_id             = var.subaccount_id
-  default_identity_provider = "custom.idp"
-  depends_on                = [btp_subaccount_trust_configuration.create_trust]
-}
-
-
-#resource "btp_subaccount_trust_configuration" "change_default_idp" {
-#  subaccount_id            = var.subaccount_id
-#  identity_provider        = "sap.default"
-#  available_for_user_logon = false
-#}
