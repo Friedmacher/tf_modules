099230a21446d90ddce085c416a37ec9f3435cd9
Manuel Friedmacher <manuel.friedmacher@outlook.com>
2026-07-20 18:45:43 +0000

    fix: remove output abap sk

--- modules/abap_add/outputs.tf
+++ modules/abap_add/outputs.tf
@@ -2,9 +2,3 @@ output "abap_dashboard_url" {
   value       = cloudfoundry_service_instance.abap_env.dashboard_url
   description = "The URL of the ABAP service instance dashboard."
 }
-
-output "abap_service_key" {
-  value       = btp_subaccount_service_binding.abap_binding.credentials
-  description = "The credentials of the ABAP service instance."
-  sensitive   = true
-}
