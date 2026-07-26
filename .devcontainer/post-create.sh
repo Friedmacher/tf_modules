84b16225f60abbef9575e4ca2efdba153ad4f940
Manuel Friedmacher <manuel.friedmacher@outlook.com>
2026-07-26 19:51:58 +0000

    change to multi repo structure

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
 set -e
+echo ""
+echo "✅ User-level configuration started."
+
+sudo mkdir -p ../repos
+sudo chown vscode:vscode ../repos
+
+echo ""
+echo "Cloneing required repositories into ../repos/"
+git clone https://github.com/Friedmacher/tf_module_build_sa.git ../repos/tf_module_build_sa
+git clone https://github.com/Friedmacher/tf_module_enable_cf.git ../repos/tf_module_enable_cf
 
 echo ""
 echo "✅ User-level configuration complete."
